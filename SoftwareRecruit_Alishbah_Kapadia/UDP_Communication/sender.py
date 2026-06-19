cbf26959d62df3d486cb640b8bffb71f40460e5f
Alishbah Kapadia <alishbahkap@gmail.com>
2026-06-19 16:56:45 -0400

    added validation for the data

--- SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/sender.py
+++ SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/sender.py
@@ -8,6 +8,21 @@ num = [random.randint(0, 65535) for _ in range(100)] # assuming unsigned so 0 to
 
 message = json.dumps(num)
 
+def validating(num):
+
+    if not isinstance(num, list):
+        return False
+    
+    for item in num:
+        if not isinstance(item, int) or item < 0 or item > 65535:
+            return False
+
+    if len(num) != 100:
+        return False    
+
+    return True
+
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.sendto(
