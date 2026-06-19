dec32ad3c4ca64d87f02fd5874175bf0338a6978
Alishbah Kapadia <alishbahkap@gmail.com>
2026-06-18 20:12:10 -0400

    test

--- SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/reciever.py
+++ SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/reciever.py
@@ -1,1 +1,14 @@
+import socket
 
+recIP = "127.0.0.1"
+port = 5555
+buff = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((recIP, port))
+
+print("Listening...")
+
+while True:
+    data, sender = sock.recvfrom(buff)
+    print("Received:", data.decode())
--- SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/sender.py
+++ SoftwareRecruit_Alishbah_Kapadia/UDP_Communication/sender.py
@@ -1,1 +1,14 @@
+import socket
 
+targIP = "127.0.0.1"
+port = 5555
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.sendto(
+    "Hello from sender".encode(),
+    (targIP, port)
+)
+
+print("Message sent")
+# 
